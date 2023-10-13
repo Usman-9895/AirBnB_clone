b811a37c64802340059510703ec97035665e6755
ebuka1017 <isaacokwuzi@gmail.com>
2023-10-12 20:36:41 -0700

    added __str__ to BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -18,6 +18,10 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
 
+    def __str__(self):
+        class_name = type(self).__name__
+        return f"[{class_name}] ({self.id}) {self.__dict__}"
+
     def validate(self):
         """Validate the attributes of the object before saving."""
         for key, value in self.__dict__.items():
